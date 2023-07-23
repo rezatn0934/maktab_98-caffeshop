395cc29799a47df90e72e58f4d3849d1db257021
rezatn0934 <rezatn0934@gmail.com>
2023-07-23 21:07:58 +0330

    feat: define postgres as default database

--- caffeshop/caffeshop/settings.py
+++ caffeshop/caffeshop/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'caffeshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'rtn093471'
     }
 }
 
