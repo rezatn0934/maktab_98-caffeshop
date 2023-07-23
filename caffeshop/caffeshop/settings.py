ad50a62314483ac712b5b33c5196b847cf35da44
rezatn0934 <rezatn0934@gmail.com>
2023-07-23 21:06:56 +0330

    feat: add apps to install_app

--- caffeshop/caffeshop/settings.py
+++ caffeshop/caffeshop/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'menu',
+    'orders',
+    'accounts'
 ]
 
 MIDDLEWARE = [
