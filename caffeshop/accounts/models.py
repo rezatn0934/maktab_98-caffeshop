5110b0b267a87004a58b57e1b5c9dda6dbba3e7c
rezatn0934 <rezatn0934@gmail.com>
2023-07-26 01:05:18 +0330

    fix: changing admin to superuser

--- caffeshop/accounts/models.py
+++ caffeshop/accounts/models.py
@@ -13,7 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=100, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'phone'
