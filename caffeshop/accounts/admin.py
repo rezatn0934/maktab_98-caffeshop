2bf62df94c10994ba5ef2a8ff3aad7b51cefe65d
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-07-25 13:03:23 +0430

    feat:admin panel customization

--- caffeshop/accounts/admin.py
+++ caffeshop/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import authenticate
+from accounts import CustomUserCreationForm, CustomUserChangeForm
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ("phone", "first_name", "last_name", "is_staff", "is_active", "is_admin",)
+    list_filter = ("phone", "first_name", "last_name", "is_staff", "is_active", "is_admin",)
+    fieldsets = (
+        (None, {"fields": ("phone", "password", "first_name", "last_name",)}),
+        ("Permissions",
+         {"fields": ("is_staff", "is_active", "is_admin", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "phone", "password1", "password2", "first_name", "last_name",
+                "is_active", "is_staff", "is_admin", "groups", "user_permissions"
+            )}
+         ),
+    )
+    search_fields = ("phone",)
+    ordering = ("phone",)
+
+admin.site.site_header = 'Cafe Management'
+admin.site.site_title = 'Coffee shop Management'
+admin.site.index_title = 'Welcome to staff panel'
+admin.site.register(User, CustomUserAdmin)
