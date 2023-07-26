644129fbe76acd3fb654b920317d9d0f8b918a1d
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-07-26 17:04:31 +0330

    feat: create Order detail model class

--- caffeshop/orders/models.py
+++ caffeshop/orders/models.py
@@ -15,3 +15,15 @@ class Order(models.Model):
         return f"Order{self.id}, order total price: {self.total_price}"
 
 
+
+
+class Order_detail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.FloatField(null=True, blank=True)
+    total_price = models.FloatField(validators=[MinValueValidator(0.0),], null=True, blank=True)
+
+    def __str__(self):
+        return f"order: {self.order}, order item total price: {self.total_price}"
+
