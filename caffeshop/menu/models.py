0a7534efba21ab15cdc3d5ea750b1a358b0208e0
shahrzadjafari <jafari.shahrzad74@gmai.com>
2023-07-26 21:00:35 +0330

    feat: change admin panel plural name

--- caffeshop/menu/models.py
+++ caffeshop/menu/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     image = models.ImageField(upload_to='images/category/', blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
 
