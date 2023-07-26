8b03e8f674aa308e0d33f383e1abb0d688cd4b99
rezatn0934 <rezatn0934@gmail.com>
2023-07-26 23:13:00 +0330

    feat: define product admin panel

--- caffeshop/menu/admin.py
+++ caffeshop/menu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['img_preview']
+    list_display = ['name', 'category', 'price_per_item', 'active', 'num_product', 'description_product', 'img_preview']
+    list_editable = ['category', 'price_per_item', 'active', 'num_product']
+    list_filter = ['name', 'category', 'price_per_item', 'active', 'num_product']
+
+    search_fields = ['name', 'category', 'price_per_item', 'active', 'num_product']
+    ordering = ['name', 'category', 'price_per_item', 'active', 'num_product']
+    list_per_page = 15
