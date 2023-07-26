03b898c3a98f0129111587a18f2dfbd223de3902
rezatn0934 <rezatn0934@gmail.com>
2023-07-26 23:23:23 +0330

    feat: creat order admin panel

--- caffeshop/orders/admin.py
+++ caffeshop/orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "phone_number", "date", "table_number", "total_price"]
+    list_filter = ["phone_number", "date", "table_number", "total_price"]
+    list_editable = ["phone_number", "table_number", "total_price"]
+
+    search_fields = ["phone_number", "date", "table_number", "total_price"]
+    ordering = ["phone_number", "date", "table_number", "total_price"]
+    list_per_page = 15
