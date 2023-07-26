1709a8bda487a1f805e7e68d860551fd6d050dd1
shahrzadjafari <jafari.shahrzad74@gmai.com>
2023-07-26 20:57:37 +0330

    feat: add category model

--- caffeshop/menu/models.py
+++ caffeshop/menu/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    image = models.ImageField(upload_to='images/category/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
