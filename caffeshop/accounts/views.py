548bd9bdbcd609401ec290427b09051f8f699bff
Farzam <ffarzam_1992@yahoo.com>
2023-07-28 17:41:49 +0330

    chore: otp verification view added

--- caffeshop/accounts/views.py
+++ caffeshop/accounts/views.py
@@ -37,3 +37,31 @@ def staff_login(request):
         form = StaffLoginForm()
 
     return render(request, "login.html", {"form": form})
+
+
+def verify(request):
+    if request.method == "POST":
+        form = VerifyCodeForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            code = cd["code"]
+            phone = request.session["staff_login_info"]["phone"]
+            code_instance = Otp_code.objects.get(phone=phone)
+
+            if code_instance:
+                if code == code_instance.code:
+                    Otp_code.objects.filter(phone=phone).delete()
+                    user = User.objects.get(phone=phone)
+                    login(request, user, backend='accounts.authentication.PhoneAuthBackend')
+                    messages.success(request, "logged in successfully", "success")
+                    return redirect("dashboard")
+                else:
+                    messages.success(request, "code is wrong", "danger")
+                    return redirect("verify")
+            else:
+                messages.success(request, "Your code has been expired", "danger")
+                return redirect("login")
+
+    else:
+        form = VerifyCodeForm()
+    return render(request, "verify.html", {"form": form})
