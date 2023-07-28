e2692f6238e107e5536e2593083474f963deed7f
Farzam <ffarzam_1992@yahoo.com>
2023-07-28 16:39:40 +0330

    chore: staff login view added

--- caffeshop/accounts/views.py
+++ caffeshop/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .form import StaffLoginForm, VerifyCodeForm
+from django.contrib.auth import login
+from .authentication import PhoneAuthBackend
+import random
+from utils import send_otp_code
+from .models import Otp_code, User
+from django.contrib import messages
+
 
 # Create your views here.
+
+
+def staff_login(request):
+    if request.method == "POST":
+        form = StaffLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = PhoneAuthBackend().authenticate(request, phone=cd["phone"], password=cd["password"])
+            if user is not None:
+                random_code = random.randint(1000, 9999)
+                send_otp_code(cd["phone"], random_code)
+                Otp_code.objects.create(phone=cd["phone"], code=random_code)
+
+                request.session["staff_login_info"] = {
+                    "phone": cd["phone"],
+                }
+
+                messages.success(request, "6-digit code was sent for you", 'success')
+                return redirect("verify")
+            else:
+
+                messages.error(request, "username and password are wrong", "danger")
+                return redirect("login")
+
+    elif request.method == "GET":
+        form = StaffLoginForm()
+
+    return render(request, "login.html", {"form": form})
