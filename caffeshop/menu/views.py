ae3f97e223c5559965ceee138fe1829660963e7e
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-07-28 23:18:27 +0330

    chore: menu - menu page view

--- caffeshop/menu/views.py
+++ caffeshop/menu/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404 , HttpResponse                        
+from .models import Product, Category
+from django.db.models import Q
+import datetime
+import json
 # Create your views here.
+
+
+def menu(request):
+    categories = get_list_or_404(Category)
+    products = get_list_or_404(Product)
+    orders = request.COOKIES.get('orders', '{}')
+    orders = orders.replace("\'", "\"")
+    orders = json.loads(orders)
+    context = {'categories': categories, 'products': products, 'number_of_order_items': sum([int(order_qnt) for order_qnt in orders.values()])}
+    html = render(request, 'menu\menu.html', context)
+    html.set_cookie('number_of_order_items', sum([int(order_qnt) for order_qnt in orders.values()]))
+    if request.method == 'GET':
+        return html
+    
