fd05577aa02c0149ad642e9b35f7bc8f086beb4a
shahrzadjafari <jafari.shahrzad74@gmai.com>
2023-07-29 17:00:41 +0330

    feat: add parent category in home.views

--- caffeshop/home/views.py
+++ caffeshop/home/views.py
@@ -8,4 +8,4 @@ def home(request):
     parentcat = ParentCategory.objects.all()
     categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'home.html', {'categories': categories, 'products': products, 'parentcategories': parentcat})
+    return render(request, 'home/home.html', {'categories': categories, 'products': products, 'parentcategories': parentcat})
