888d3c85d61de02623cda6ee840f8822ca690ce9
shahrzadjafari <jafari.shahrzad74@gmai.com>
2023-07-29 15:49:31 +0330

    feat:home.views: add parent category

--- caffeshop/home/views.py
+++ caffeshop/home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from menu.models import Category, Product
+from menu.models import Category, Product,ParentCategory
 
 # Create your views here.
 
 
 def home(request):
+    parentcat = ParentCategory.objects.all()
     categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'home.html', {'categories': categories, 'products': products})
+    return render(request, 'home.html', {'categories': categories, 'products': products, 'parentcategories': parentcat})
