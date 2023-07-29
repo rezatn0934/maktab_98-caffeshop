13a40c647678a389f2842eb38a6ee78e093faa96
shahrzadjafari <jafari.shahrzad74@gmai.com>
2023-07-29 15:40:59 +0330

    feat: add function for load hompage

--- caffeshop/home/views.py
+++ caffeshop/home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from menu.models import Category, Product
+
+# Create your views here.
+
+
+def home(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    return render(request, 'home.html', {'categories': categories, 'products': produc
