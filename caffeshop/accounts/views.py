233ac2cf27ba5d7717d92748d88ea3feb7e54e1f
Farzam <ffarzam_1992@yahoo.com>
2023-07-30 18:30:02 +0330

    refactor: refactoring staff login view

--- caffeshop/accounts/views.py
+++ caffeshop/accounts/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from .form import StaffLoginForm, VerifyCodeForm
 from django.contrib.auth import login
 from .authentication import PhoneAuthBackend
-import random
-from utils import send_otp_code
-from .models import Otp_code, User
+from .models import User
 from django.contrib import messages
 
 
@@ -12,31 +10,23 @@ from django.contrib import messages
 
 
 def staff_login(request):
+    message = None
     if request.method == "POST":
         form = StaffLoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             user = PhoneAuthBackend().authenticate(request, phone=cd["phone"], password=cd["password"])
             if user is not None:
-                random_code = random.randint(10000000, 99999999)
-                # send_otp_code(cd["phone"], random_code)
-                Otp_code.objects.create(phone=cd["phone"], code=random_code)
-
-                request.session["staff_login_info"] = {
-                    "phone": cd["phone"],
-                }
-
-                messages.success(request, "8-digit code was sent for you", 'success')
+                request.session["phone"] = cd["phone"]
                 return redirect("verify")
             else:
-
-                messages.error(request, "username and password are wrong", "danger")
-                return redirect("login")
+                message = "Invalid phone number or password"
 
     elif request.method == "GET":
-        form = StaffLoginForm()
-
-    return render(request, "login.html", {"form": form})
+        if request.user.is_authenticated:
+            return redirect("dashboard")
+    form = StaffLoginForm()
+    return render(request, "login.html", {"message": message, "form": form})
 
 
 def verify(request):
