726a4cf7d35b5b2c923f7713594205505409b31c
Farzam <ffarzam_1992@yahoo.com>
2023-07-31 00:35:16 +0330

    fix: value error message fixed

--- caffeshop/accounts/manager.py
+++ caffeshop/accounts/manager.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import BaseUserManager
 class UserManager(BaseUserManager):
     def create_user(self, phone, password):
         if not phone:
-            raise ValueError("Users must have an email address")
+            raise ValueError("Users must have an phone number")
 
         user = self.model(phone=phone)
         user.set_password(password)
