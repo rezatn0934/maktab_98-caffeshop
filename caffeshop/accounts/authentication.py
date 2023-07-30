0aea84f911c5af73e8356fe1794efff80e00c50b
Farzam <ffarzam_1992@yahoo.com>
2023-07-30 18:12:03 +0330

    refactor: removing password from authentication

--- caffeshop/accounts/authentication.py
+++ caffeshop/accounts/authentication.py
@@ -7,10 +7,7 @@ class PhoneAuthBackend(ModelBackend):
     def authenticate(self, request, phone=None, password=None, **kwargs):
         try:
             user = User.objects.get(phone=phone)
-            if user.check_password(password):
-                return user
-            else:
-                return
+            return user
         except User.DoesNotExist:
             return
 
