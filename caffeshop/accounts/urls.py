2ad3271bb6139b0a361205a1243a28c2cb843c38
Farzam <ffarzam_1992@yahoo.com>
2023-07-30 18:45:57 +0330

    chore: logout url added

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('staff_login/', views.staff_login, name='login'),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('verify/', views.verify, name="verify"),
-    path('', include('menu.urls'))
-
-
+    path('', include('menu.urls')),
+    path('logout/', views.logout_view, name="logout"),
 ]
