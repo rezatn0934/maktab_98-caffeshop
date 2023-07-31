8b674761ccd2ffdd0e55b0866f39386ac6c8cbcf
Farzam <ffarzam_1992@yahoo.com>
2023-07-31 14:36:49 +0330

    feat: Background image model added

--- caffeshop/home/models.py
+++ caffeshop/home/models.py
@@ -13,3 +13,13 @@ class Gallery(models.Model):
     def img_preview(self):
         if self.image:
             return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
+
+
+class BackgroundImage(models.Model):
+    title = models.CharField(max_length=250, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/HomePageBackground')
+
+    def img_preview(self):
+        if self.image:
+            return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
