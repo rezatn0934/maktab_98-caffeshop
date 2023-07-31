acfb9835e635b87e46fd4ca9afc096f0c8fcb59a
rezatn0934 <rezatn0934@gmail.com>
2023-07-31 16:10:06 +0330

    refac: signal - modifying the delete image function to handle only delete signals

--- caffeshop/caffeshop/signals.py
+++ caffeshop/caffeshop/signals.py
@@ -1,11 +1,7 @@
 import os
 
 
-def delete_image_file(sender, instance, **kwargs):
-    old_image = sender.objects.filter(pk=instance.pk)
-    if old_image:
-        old_image = old_image.get(pk=instance.pk).image
-    new_image = instance.image
-    if not old_image == new_image:
-        if os.path.isfile(old_image.path):
-            os.remove(old_image.path)
+def delete_image_file(sender, **kwargs):
+    img = kwargs['instance']
+    if os.path.exists(img.image.path):
+        os.remove(img.image.path)
