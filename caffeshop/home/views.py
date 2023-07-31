d8916196938f15d5635f058c06ced8c24206ae03
Farzam <ffarzam_1992@yahoo.com>
2023-07-31 14:41:00 +0330

    chore: gallery & background query added

--- caffeshop/home/views.py
+++ caffeshop/home/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from menu.models import Category, Product,ParentCategory
+from menu.models import Category, Product, ParentCategory
+from .models import Gallery, BackgroundImage
+
 
 # Create your views here.
 
 
 def home(request):
+    gallery = Gallery.objects.filter(is_active=True)
+    background_image = BackgroundImage.objects.get(is_active=True)
     parentcat = ParentCategory.objects.all()
     categories = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'home/home.html', {'categories': categories, 'products': products, 'parentcategories': parentcat})
+    context = {'categories': categories, 'products': products,
+               'parentcategories': parentcat, 'gallery': gallery,
+               "background_image": background_image}
+    return render(request, 'home/home.html', context=context)
