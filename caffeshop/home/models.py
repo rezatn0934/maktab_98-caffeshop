7ca4631dc4e07a5c5b34e54d509ed370150ef5a1
Farzam <ffarzam_1992@yahoo.com>
2023-07-31 14:35:25 +0330

    feat: Gallery model added

--- caffeshop/home/models.py
+++ caffeshop/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.html import mark_safe
+
 
 # Create your models here.
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=250, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='images/gallery')
+
+    def img_preview(self):
+        if self.image:
+            return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
