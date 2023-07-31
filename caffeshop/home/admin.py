e74b212ba11f873b88a572212eab6cea477d9512
Farzam <ffarzam_1992@yahoo.com>
2023-07-31 14:43:46 +0330

    chore: gallery admin added

--- caffeshop/home/admin.py
+++ caffeshop/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+@admin.register(models.Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    readonly_fields = ["img_preview"]
+    list_display = ["title", "img_preview", "is_active", ]
+    list_editable = ["is_active"]
