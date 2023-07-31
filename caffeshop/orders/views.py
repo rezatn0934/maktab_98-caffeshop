7b8d1c5bf9054373008b9402409707363b087bb0
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-07-31 19:11:36 +0330

    chore:  update and delete methods of product in cart added

--- caffeshop/orders/views.py
+++ caffeshop/orders/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse , HttpResponseRedirect , redirect
 from .models import Order, Order_detail
 from menu.models import Product, Category, ParentCategory
+from django.urls import reverse
 import json
 # Create your views here.
 
-def cart(request):
+def cart1(request):
     orders = request.COOKIES.get('orders', '{}')
     orders = orders.replace("\'", "\"")
     orders = json.loads(orders)
-    print('orders: ',orders)
-    print('type orders: ',type(orders))
-
-    order_items = [(Product.objects.get(id=product_id), quantity, Product.objects.get(id=product_id).price_per_item  * quantity) for product_id, quantity in orders.items()]
+    updated_orders = orders.copy()
+    order_items = []
+    for product_id, quantity in orders.items():
+        qs = Product.objects.filter(id=product_id)
+        if qs.exists():
+            obj = qs.get(id=product_id)
+            tp =  obj.price_per_item * int(quantity)        
+            order_items.append((obj , quantity, tp))
+        else :
+            updated_orders.pop(product_id)
+    order_total_price = sum(map(lambda item: int(item[2]), order_items))
+    response = render(request, 'orders/cart.html', {'order_items':order_items, 'order_total_price': order_total_price})
+    response.set_cookie('orders', updated_orders)
+    response.set_cookie('number_of_order_items', sum([int(order_qnt) for order_qnt in updated_orders.values()]))
     if request.method == 'GET':
-        return render(request, 'orders/cart.html', {'order_items':order_items})
+        return response
+
+    
+def update_or_remove(request):
+    if request.method == 'POST':
+        orders = request.COOKIES.get('orders', '{}')
+        orders = orders.replace("\'", "\"")
+        orders = json.loads(orders)
+        updated_orders = orders.copy()
+        if request.POST.get('update'):
+            updated_orders[request.POST.get('product')] = request.POST.get('quantity')
+        if request.POST.get('remove'):
+            updated_orders.pop(request.POST.get('product'))
+        response = redirect('orders:cart')
+        response.set_cookie('orders', updated_orders)
+        response.set_cookie('number_of_order_items', sum([int(order_qnt) for order_qnt in updated_orders.values()]))
+        return response
