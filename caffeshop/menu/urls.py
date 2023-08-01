f9c2c063af2feb82fc8f46fbfe993471f8a970a3
rezatn0934 <rezatn0934@gmail.com>
2023-08-01 22:52:35 +0330

    refac: Menu - refactor search url path name

--- caffeshop/menu/urls.py
+++ caffeshop/menu/urls.py
@@ -7,5 +7,5 @@ app_name = "menu"
 urlpatterns = [
     path('menu/', views.menu, name='menu'),
     path('show_product/<str:name>', views.product, name='show_product'),
-    path('search_product/', views.search_product_view, name='search'),
+    path('search/', views.search_product_view, name='search'),
 ]
