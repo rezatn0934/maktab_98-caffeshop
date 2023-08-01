9e9561fa1373e9236644035b0c774cb67aaca887
rezatn0934 <rezatn0934@gmail.com>
2023-08-02 01:40:18 +0330

    chore: Home - define custom delete object action

--- caffeshop/home/admin.py
+++ caffeshop/home/admin.py
@@ -86,3 +86,23 @@ class AboutAdmin(admin.ModelAdmin):
     list_display = ['title', 'truncated_content', 'img_preview', 'is_active', ]
     list_editable = ['is_active']
 
+    @admin.action(description='Delete selected about  ')
+    def delete_about_image(self, request, queryset):
+        count = 0
+        for query in queryset:
+            if query.is_active:
+                self.message_user(
+                    request,
+                    f'{query.title} about can not be deleted.',
+                    messages.ERROR,
+                )
+            else:
+                count += 1
+                query.delete()
+
+        if count:
+            self.message_user(
+                request,
+                f'{count} selected about were successfully deleted.',
+            )
+
