e99fc0623f9188a1cb0ceddbb26d0caa0eecf7d3
rezatn0934 <rezatn0934@gmail.com>
2023-08-02 00:31:03 +0330

    refac: Home - overwrite save method to change media files

--- caffeshop/home/models.py
+++ caffeshop/home/models.py
@@ -103,6 +103,17 @@ class About(models.Model):
         if self.image:
             return mark_safe(f'<img src="{self.image.url}" width="50" height="80"/>')
 
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_instance = About.objects.get(pk=self.pk)
+            if not old_instance.image == self.image:
+                if old_instance.image:
+                    if os.path.exists(old_instance.image.path):
+                        os.remove(old_instance.image.path)
+        About.objects.all().update(is_active=False)
+        self.is_active = True
+        super().save(*args, **kwargs)
+
     def delete(self, *args, **kwargs):
         if self.image:
             if os.path.exists(self.image.path):
