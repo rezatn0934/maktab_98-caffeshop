b221a8c088137422319e0c57b21681c78252a379
Farzam <ffarzam_1992@yahoo.com>
2023-08-06 01:19:27 +0330

    refactor: change product model

--- caffeshop/orders/models.py
+++ caffeshop/orders/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from menu.models import Product, Category
-from django.core.validators import RegexValidator, MinValueValidator
+from django.core.validators import MinValueValidator
+from utils import phoneNumberRegex
+
+
 # Create your models here.
 
 
 class Order(models.Model):
-    status_choices = (('Customer_confirm', (('UC', 'Undetermined'), ('AC', 'Approved'), ('CC', 'Canceled'))),
-                      ('Staff_confirm', (('US', 'Undetermined'), ('AS', 'Approved'), ('CS', 'Canceled'))),
-                      ('delivery_confirm', (('UD', 'Undetermined'), ('AD', 'Approved'), ('CD', 'Canceled'))))
-    phoneNumberRegex = RegexValidator(regex=r"^09\d{9}$")
-    phone_number = models.CharField(validators=[phoneNumberRegex], max_length=12)
-    date = models.DateTimeField(auto_now=True)
-    table_number = models.PositiveIntegerField(validators=[MinValueValidator(0.0)], null=True, blank=True)
-    total_price = models.FloatField(validators=[MinValueValidator(0.0)], null=True, blank=True)
-    status = models.CharField(choices=status_choices, default=('Customer_confirm', (('UC', 'Undetermined'))))
+    payment_status = [("U", "unpaid"), ("P", "payed")]
+    status_choices = [('P', 'Processing'), ('A', 'Approved'), ('C', 'Canceled')]
+    phone_number = models.CharField(validators=[phoneNumberRegex], max_length=11)
+    order_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modify = models.DateTimeField(auto_now=True, editable=False)
+    table_number = models.PositiveIntegerField(validators=[MinValueValidator(0.0)])
+    status = models.CharField(max_length=1, choices=status_choices, default="P")
+    payment = models.CharField(max_length=1, choices=payment_status, default="U")
 
     def __str__(self):
         return f"Order{self.id}, order total price: {self.total_price}"
-
-
 
 
 class Order_detail(models.Model):
@@ -34,4 +34,3 @@ class Order_detail(models.Model):
 
     def __str__(self):
         return f"order: {self.order}, order item total price: {self.total_price}"
-
