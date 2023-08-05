ffadf62fc7469a10194446c0d348701ecb78cf89
Farzam <ffarzam_1992@yahoo.com>
2023-08-05 17:12:02 +0330

    refactor: set class view for verify urls

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('staff_login/', views.StaffLogin.as_view(), name='login'),
     path('dashboard/', views.dashboard, name="dashboard"),
-    path('verify/', views.verify, name="verify"),
+    path('verify/', views.Verify.as_view(), name="verify"),
     path('logout/', views.logout_view, name="logout"),
 ]
