02990ae50b0100b22810f87c958d62f488f0c993
Farzam <ffarzam_1992@yahoo.com>
2023-08-05 16:48:38 +0330

    refactor: set class view for staff login urls

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('staff_login/', views.staff_login, name='login'),
+    path('staff_login/', views.StaffLogin.as_view(), name='login'),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('verify/', views.verify, name="verify"),
     path('logout/', views.logout_view, name="logout"),
