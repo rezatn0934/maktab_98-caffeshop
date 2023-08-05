b651208cf7f803d1d82cf3ac5c4b60760063ba7b
Farzam <ffarzam_1992@yahoo.com>
2023-08-05 18:03:08 +0330

    refactor: delete background image model

--- caffeshop/home/models.py
+++ caffeshop/home/models.py
@@ -36,36 +36,6 @@ class Gallery(models.Model):
         return self.title or ''
 
 
-class BackgroundImage(models.Model):
-    title = models.CharField(max_length=50, null=True, blank=True)
-    is_active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='images/HomePageBackground')
-
-    def img_preview(self):
-        if self.image:
-            return mark_safe(f'<img src="{self.image.url}" width="150" height="150"/>')
-
-    def delete(self, *args, **kwargs):
-        if self.image:
-            if os.path.exists(self.image.path):
-                os.remove(self.image.path)
-        super().delete(*args, **kwargs)
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            old_instance = BackgroundImage.objects.get(pk=self.pk)
-            if not old_instance.image == self.image:
-                if old_instance.image:
-                    if os.path.exists(old_instance.image.path):
-                        os.remove(old_instance.image.path)
-        BackgroundImage.objects.all().update(is_active=False)
-        self.is_active = True
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title or ''
-
-
 class Info(models.Model):
     cafe_title = models.CharField(max_length=25)
     cafe_motto = models.TextField()
@@ -84,7 +54,6 @@ class Logo(models.Model):
     title = models.CharField(max_length=50, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     image = models.ImageField(upload_to='images/logo')
-
 
     def img_preview(self):
         if self.image:
