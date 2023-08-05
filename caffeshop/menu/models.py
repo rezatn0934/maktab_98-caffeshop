9d9065cecf8f699b3fdffb8a4c8a5f314e134a9e
Farzam <ffarzam_1992@yahoo.com>
2023-08-05 23:36:48 +0330

    refactor: delete parent category model

--- caffeshop/menu/models.py
+++ caffeshop/menu/models.py
@@ -2,37 +2,9 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.html import mark_safe
 import os
+
+
 # Create your models here.
-
-
-class ParentCategory(models.Model):
-    name = models.CharField(max_length=250, unique=True)
-    image = models.ImageField(upload_to='images/parent_category', blank=True, null=True)
-
-    class Meta:
-        verbose_name_plural = "ParentCategories"
-
-    def img_preview(self):
-        if self.image:
-            return mark_safe(f'<img src = "{self.image.url}" width = "150" height="150"/> ')
-
-    def delete(self, *args, **kwargs):
-        if self.image:
-            if os.path.exists(self.image.path):
-                os.remove(self.image.path)
-        super().delete(*args, **kwargs)
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            old_instance = ParentCategory.objects.get(pk=self.pk)
-            if not old_instance.image == self.image:
-                if old_instance.image:
-                    if os.path.exists(old_instance.image.path):
-                        os.remove(old_instance.image.path)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
 
 
 class Category(models.Model):
