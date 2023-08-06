343f9d79233b35573b38b32351d90a0da0e4bb6e
rezatn0934 <rezatn0934@gmail.com>
2023-08-07 00:10:29 +0330

    refac: Accounts - refactor accounts url

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('staff_login/', views.StaffLogin.as_view(), name='login'),
-    path('dashboard/', views.Dashboard.as_view, name="dashboard"),
+    path('login/', views.StaffLogin.as_view(), name='login'),
+    path('dashboard/', views.Dashboard.as_view(), name="dashboard"),
     path('verify/', views.Verify.as_view(), name="verify"),
     path('logout/', views.logout_view, name="logout"),
 ]
