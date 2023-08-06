c39393aaef8e32fd83d184b673902bb3f024150f
rezatn0934 <rezatn0934@gmail.com>
2023-08-07 00:04:47 +0330

    refac: Accounts - refactor Dashboard url

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('staff_login/', views.StaffLogin.as_view(), name='login'),
-    path('dashboard/', views.dashboard, name="dashboard"),
+    path('dashboard/', views.Dashboard.as_view, name="dashboard"),
     path('verify/', views.Verify.as_view(), name="verify"),
     path('logout/', views.logout_view, name="logout"),
 ]
