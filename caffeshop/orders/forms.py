f59ec4ea376ba4a992fc60c415b6a0ca6eef2922
Farzam <ffarzam_1992@yahoo.com>
2023-08-06 17:54:00 +0330

    refactor: change order form

--- caffeshop/orders/forms.py
+++ caffeshop/orders/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from .models import Table
+
 
 class OrderForm(forms.Form):
     error_css_class = "error"
     required_css_class = "required"
-    table_number = forms.IntegerField(max_value=15, min_value=1, widget=forms.NumberInput(attrs={"id":"select-box"}))
-    phone = forms.CharField(max_length=11, widget= forms.TextInput(attrs={"id": "phone-input"}), required=True)
+    table_number = forms.IntegerField(max_value=len(Table.objects.all()), min_value=1, widget=forms.NumberInput(attrs={"id": "checkbox"}))
+    phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs={"id": "phone-input"}), required=True)
