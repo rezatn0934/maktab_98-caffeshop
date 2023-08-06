5ddbe461cb9e73644871a87047d52ae1824206d4
Farzam <ffarzam_1992@yahoo.com>
2023-08-06 19:03:31 +0330

    refactor: change order form

--- caffeshop/orders/forms.py
+++ caffeshop/orders/forms.py
@@ -5,7 +5,7 @@ from .models import Order, Table
 class OrderForm(forms.ModelForm):
     error_css_class = "error"
     required_css_class = "required"
-    table_number = forms.ModelChoiceField(queryset=Table.objects.all())
+    table_number = forms.ModelChoiceField(queryset=Table.objects.filter(occupied=False))
 
     # phone = forms.CharField(max_length=11, widget=forms.TextInput(attrs={"id": "phone-input"}), required=True)
     class Meta:
