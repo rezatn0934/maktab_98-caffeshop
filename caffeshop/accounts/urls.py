65f348e45a0513ddd3f22d5633f93c0143b3a93b
rezatn0934 <rezatn0934@gmail.com>
2023-08-08 12:12:18 +0330

    chore: Accounts - add cancel order view to url

--- caffeshop/accounts/urls.py
+++ caffeshop/accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name="logout"),
     path('orders/', views.Orders.as_view(), name='order_list'),
     path('confirm_order/<int:pk>', views.confirm_order, name='confirm_order'),
+    path('cancel_order/<int:pk>', views.cancel_order, name='cancel_order'),
     path('delete_order_item/<int:pk>', views.delete_order_detail, name='delete_order_item'),
 ]
