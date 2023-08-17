8572d7cfe8e944aff7ecdaa54e7dccb36019ea04
rezatn0934 <rezatn0934@gmail.com>
2023-08-17 23:52:48 +0330

    chore : define test method for Login view

---
+++ caffeshop/accounts/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from accounts import views
+
+
+class TestUrls(SimpleTestCase):
+    def test_login(self):
+        url = reverse('login')
+        self.assertEqual(resolve(url).func.view_class, views.StaffLogin)
