288651228995af97a96c4feb81146e5fbc2e406e
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-08-18 07:56:06 +0430

    feat:urls of cart and create_order view added

---
+++ caffeshop/orders/tests/test_urls.py
@@ -0,0 +1,12 @@
+from django.urls import reverse, resolve
+from orders.views import *
+from django.test import SimpleTestCase
+
+class TestUrls(SimpleTestCase):
+    def test_cart_view(self):
+        url=reverse('orders:cart')
+        self.assertEquals(resolve(url).func.view_class, CartView)
+
+    def test_create_order_view(self):
+        url=reverse('orders:create_order')
+        self.assertEquals(resolve(url).func, create_order)
