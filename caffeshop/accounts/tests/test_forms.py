011ecd92e5cf8ad0eaa0d16d666370758dfc395f
rezatn0934 <rezatn0934@gmail.com>
2023-08-18 14:47:13 +0330

    chore : define test method for Custom User Creation Form in case of exists username

--- caffeshop/accounts/tests/test_forms.py
+++ caffeshop/accounts/tests/test_forms.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
-from accounts.form import CustomUserCreationForm
+from accounts.form import CustomUserCreationForm, CustomUserChangeForm
 from accounts.models import User
 
 
 class TestUserCreationForm(TestCase):
+
     def test_valid_data(self):
         form = CustomUserCreationForm(
             data={'phone': '09198470934', 'first_name': 'reza', 'last_name': 'teymouri', 'password1': 'reza123456',
@@ -13,3 +14,11 @@ class TestUserCreationForm(TestCase):
     def test_empty_form(self):
         form = CustomUserCreationForm()
         self.assertFalse(form.is_valid())
+
+    def test_clean_username(self):
+        User.objects.create_user(phone='09198470934', password='rtn093471')
+        form = CustomUserCreationForm(
+            data={'phone': '09198470934', 'first_name': 'reza', 'last_name': 'teymouri', 'password1': 'reza123456',
+                  'password2': 'reza123456'})
+        self.assertEqual(len(form.errors), 1)
+        self.assertTrue(form.has_error)
