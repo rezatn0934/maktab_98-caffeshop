8d40bac5954f63127861613dc282707b89b883d1
rezatn0934 <rezatn0934@gmail.com>
2023-08-19 19:20:59 +0330

    chore : define test class for Verify view in case of GET method and In Case of anonymous user add sessions

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AnonymousUser
+from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 
 from accounts.models import User
-from accounts.form import CustomUserCreationForm
+from accounts.form import CustomUserCreationForm, VerifyCodeForm
 from accounts.views import (
     StaffLogin,
     Verify,
@@ -58,14 +59,20 @@ class TestVerify(TestCase):
         )
         self.factory = RequestFactory()
 
-    def test_staff_login_GET_authenticate(self):
+    def test_verify_GET_authenticate(self):
         request = self.factory.get(reverse('verify'))
         request.user = self.user
-        response = StaffLogin.as_view()(request)
+        response = Verify.as_view()(request)
         self.assertEqual(response.status_code, 302)
 
-    def test_staff_login_GET_anonymous(self):
-        request = self.factory.get(reverse('login'))
+    def test_verify_GET_anonymous(self):
+        request = self.factory.get(reverse('verify'))
         request.user = AnonymousUser()
-        response = StaffLogin.as_view()(request)
+        middleware = SessionMiddleware(lambda request: None)
+        middleware.process_request(request)
+        request.session['phone'] = '09038916990'
+        request.session.save()
+
+        response = Verify.as_view()(request)
         self.assertEqual(response.status_code, 200)
+        del request.session['phone']
