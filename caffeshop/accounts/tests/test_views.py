ae5f1d6986b28e222a853a7d00f24e944821aa63
rezatn0934 <rezatn0934@gmail.com>
2023-08-19 15:48:10 +0330

    chore : define test class for Verify view in case of GET method and In Case of authenticated user

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -6,6 +6,7 @@ from accounts.models import User
 from accounts.form import CustomUserCreationForm
 from accounts.views import (
     StaffLogin,
+    Verify,
 )
 
 
@@ -32,14 +33,33 @@ class TestStaffLogin(TestCase):
         response = StaffLogin.as_view()(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_staff_login_POST_valid(self):
-        response = self.client.post(reverse('login'), data={'phone': '09038916990'})
-        self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, reverse('verify'))
+    # def test_staff_login_POST_valid(self):
+    #     response = self.client.post(reverse('login'), data={'phone': '09038916990'})
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertRedirects(response, reverse('verify'))
 
     def test_staff_login_POST_invalid(self):
         response = self.client.post(reverse('login'), data={'phone': 'gfhbjnh'})
         self.assertEqual(response.status_code, 200)
         self.failIf(response.context['form'].is_valid())
-        self.assertEqual(response.context['message'], 'Wrong input, Phone number Should Start 11 digits Like 09*********')
+        self.assertEqual(response.context['message'],
+                         'Wrong input, Phone number Should Start 11 digits Like 09*********')
         self.assertFormError(form=response.context['form'], field='phone', errors='Enter a valid value.')
+
+
+class TestVerify(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(
+            phone='09038916990',
+            password='reza123456',
+            first_name='reza',
+            last_name='teymouri'
+        )
+        self.factory = RequestFactory()
+
+    def test_staff_login_GET_authenticate(self):
+        request = self.factory.get(reverse('verify'))
+        request.user = self.user
+        response = StaffLogin.as_view()(request)
+        self.assertEqual(response.status_code, 302)
