90844d13e949fe377929be43ea1986432283dd4f
Farzam <ffarzam_1992@yahoo.com>
2023-08-19 10:52:31 +0330

    chore: add failed save test for info model

--- caffeshop/home/tests/test_models.py
+++ caffeshop/home/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from home.models import About, Gallery, Info
 from model_bakery import baker
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.conf import settings
 import os
@@ -32,4 +34,16 @@ class TestInfoModel(TestCase):
         self.assertEqual(len(objs), 1)
         self.assertEqual(len(obj_f), 1)
         self.assertEqual(obj_g.cafe_title, "farzam")
+
+    def test_model_failed_save(self):
+        failed_save_info = baker.make(Info, cafe_title="reza")
+        objs = Info.objects.all()
+        obj_f = Info.objects.filter(cafe_title="reza")
+        obj_g = Info.objects.get(cafe_title="farzam")
+        self.assertEqual(obj_g.cafe_title, "farzam")
+        self.assertTrue(len(objs) == 1)
+        self.assertEqual(len(obj_f), 0)
+        with self.assertRaises(Http404):
+            get_object_or_404(Info, cafe_title="reza")
+
 
