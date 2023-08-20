ee9d6d252126e84dac436b037e39ef67afb76776
rezatn0934 <rezatn0934@gmail.com>
2023-08-21 01:48:15 +0330

    chore: define tearDown for Cancel order view test class

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -586,3 +586,10 @@ class TestConfirmOrder(TestCase):
             password=self.password,
         )
         self.manager_group = Group.objects.get(name='Managers')
+
+    def tearDown(self):
+        Order_detail.objects.all().delete()
+        Product.objects.all().delete()
+        Order.objects.all().delete()
+        Table.objects.all().delete()
+        self.user.delete()
