ba1479190830a2ff203d225ab5a50a90fb673715
nima-mojarradi <nima.mojarradi83@gmail.com>
2023-08-20 18:04:28 +0430

    feat:test for total price added

--- caffeshop/orders/tests/test_models.py
+++ caffeshop/orders/tests/test_models.py
@@ -43,13 +43,13 @@ class TestOrdersModels(TestCase):
         )
 
     def test_get_order_items(self):
-        # order = Order.objects.create(payment='P',status='A',phone_number='09152593858',table_number=Table.objects.create(
-        # name='orchid',Table_number=4,occupied=True))
-        # order_items = Order_detail.objects.create(order=order,
-        # product=Product.objects.create(category=Category.objects.create(name='Drinks'),name='Tea',description = 'djf;adjf;ak', price=5.00),
-        # quantity=1,price=10.00)
-        # order_products = Order_detail.objects.get(order=order.id)
-        # self.assertEqual(order_products, order_items)
+        order = Order.objects.create(payment='P',status='A',phone_number='09152593858',table_number=Table.objects.create(
+        name='orchid',Table_number=4,occupied=True))
+        order_items = Order_detail.objects.create(order=order,
+        product=Product.objects.create(category=Category.objects.create(name='Drinks'),name='Tea',description = 'djf;adjf;ak', price=5.00),
+        quantity=1,price=10.00)
+        order_products = Order_detail.objects.get(order=order.id)
+        self.assertEqual(order_products, order_items)
         order_items = self.order.get_order_items
         self.assertEqual(order_items.count(), 2)
         self.assertEqual(order_items[0].product.name, 'rice')
@@ -60,10 +60,21 @@ class TestOrdersModels(TestCase):
             payment = 'P',
             status = 'A',
             phone_number ='09152593858',
-            table_number = Table.objects.create(name='rose', Table_number = 20, occupied=True)
+            table_number = Table.objects.create(name='rose', Table_number = 20, occupied=True),
+            order_date = '2023-07-31'
         )
     def test_str(self):
-        self.assertEquals(str(self.order), 'Order5')
+        self.assertEquals(str(self.order), 'Order6')
+
+    def test_total_price(self):
+        order = Order.objects.create(
+            status = 'A',
+            payment = 'P',
+            phone_number = '09152593858',
+            table_number = Table.objects.create(name='violet', Table_number=60, occupied = True)
+        )
+        order_detail = Order_detail.objects.create(order=order,product=Product.objects.get(name='hamburger'),quantity=2, price=10.00)
+        self.assertEqual(order_detail.total_price, 20.0)
 
 
       
