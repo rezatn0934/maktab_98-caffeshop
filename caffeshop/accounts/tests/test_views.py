e9c26f8adcd9bd3ffdeb96df352259643ca0e48d
rezatn0934 <rezatn0934@gmail.com>
2023-08-22 00:02:23 +0330

    chore: define tearDown in TestOrderStatusReport class

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -1395,3 +1395,10 @@ class TestOrderStatusReport(TestCase):
             password=self.password,
         )
         self.manager_group = Group.objects.get(name='Managers')
+
+    def tearDown(self):
+        Order_detail.objects.all().delete()
+        Product.objects.all().delete()
+        Order.objects.all().delete()
+        Table.objects.all().delete()
+        self.user.delete()
