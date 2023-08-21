1741d91dda648eadcc398a69694b8a25ac057171
rezatn0934 <rezatn0934@gmail.com>
2023-08-21 12:53:26 +0330

    chore: define setUpTestData in Most Peak Business Hour view test class

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -759,3 +759,14 @@ class TestMostPopular(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['query_set']), 2)
+
+
+class TestPeakBusinessHour(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        content_type = ContentType.objects.get_for_model(Order_detail)
+        order_detail_permission = Permission.objects.filter(content_type=content_type)
+
+        manager_group, created = Group.objects.get_or_create(name="Managers")
+        manager_group.permissions.add(*order_detail_permission)
