f685301ca2c8464558ad423a999d69bb11550497
rezatn0934 <rezatn0934@gmail.com>
2023-08-21 12:38:53 +0330

    chore: define tearDown in Most Popular item view test class

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -730,3 +730,9 @@ class TestMostPopular(TestCase):
         )
         self.manager_group = Group.objects.get(name='Managers')
 
+    def tearDown(self):
+        Order_detail.objects.all().delete()
+        Product.objects.all().delete()
+        Order.objects.all().delete()
+        Table.objects.all().delete()
+        self.user.delete()
