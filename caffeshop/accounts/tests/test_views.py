4f98b074797374a1331c953509d83d1ab9c87b13
rezatn0934 <rezatn0934@gmail.com>
2023-08-21 23:08:35 +0330

    chore: define setUpTestData in TestSalesByCategory class

--- caffeshop/accounts/tests/test_views.py
+++ caffeshop/accounts/tests/test_views.py
@@ -1302,3 +1302,14 @@ class TestCustomerDemographic(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(float(customer_sales['spent']), 300.0)
+
+
+class TestSalesByCategory(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        content_type = ContentType.objects.get_for_model(Order_detail)
+        order_detail_permission = Permission.objects.filter(content_type=content_type)
+
+        manager_group, created = Group.objects.get_or_create(name="Managers")
+        manager_group.permissions.add(*order_detail_permission)
