548501ec7edc9a083227bb7ccaab60202f80a6bf
rezatn0934 <rezatn0934@gmail.com>
2023-08-22 21:34:03 +0330

    chore: define test_order_customer_count_section_url in TestOrderAdmin

--- caffeshop/orders/tests/test_admin.py
+++ caffeshop/orders/tests/test_admin.py
@@ -20,3 +20,13 @@ class TestOrderAdmin(TestCase):
         self.order_detail2.delete()
         self.order_detail3.delete()
         self.order1.delete()
+
+    def test_order_customer_count_section_url(self):
+        order_admin = OrderAdmin(Order, AdminSite())
+        expected_link = f'<a href="/admin/orders/order_detail/?order__id={self.order1.id}">{len(Order_detail.objects.filter(order=self.order1))}</a>'
+        query_set = Order.objects.filter(id=self.order1.id).annotate(
+            customer_count=Count('order_detail')
+        )
+        order = query_set.get()
+        given_link = order_admin.customer_count(order)
+        self.assertEqual(expected_link, given_link)
