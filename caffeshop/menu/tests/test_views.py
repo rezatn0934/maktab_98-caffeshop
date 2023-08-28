dcda5c2043f8e72229d2f5842e0ec7994394db1a
Farzam <ffarzam_1992@yahoo.com>
2023-08-28 12:27:08 +0330

    chore: update search tests

--- caffeshop/menu/tests/test_views.py
+++ caffeshop/menu/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from menu.models import Product, Category
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.conf import settings
 
 
 class TestMenuView(TestCase):
@@ -19,9 +21,13 @@ class TestMenuView(TestCase):
 class TestSearchView(TestCase):
 
     def setUp(self):
+        self.image = open(settings.MEDIA_ROOT / "images/test/pina_colada.png", 'rb').read()
         self.category = Category.objects.create(name='Food')
-        self.product = Product.objects.create(name='pepperoni', description="deliciouse", category=self.category,
-                                              price=10)
+        self.product = Product.objects.create(name='pepperoni', category=self.category, price=100,
+                                              image=SimpleUploadedFile.from_dict(
+                                                  {'filename': 'product_pic.png', 'content': self.image,
+                                                   'content_tye': 'image/png'})
+                                              )
         self.client = Client()
 
     def tearDown(self):
@@ -31,35 +37,18 @@ class TestSearchView(TestCase):
     def test_search_get_no_data(self):
         response = self.client.get(reverse('menu:search'))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['message'], None)
-        self.assertFalse(response.context['search_result'])
+        self.assertEqual(response.context['items'], [])
         self.assertTemplateUsed('menu/search.html')
-        self.assertTemplateNotUsed('menu/menu.html')
-        self.assertTemplateNotUsed('home/home.html')
 
     def test_search_get_invalid_search_data(self):
         response = self.client.get(reverse('menu:search'), data={'search': '111111'})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['message'], 'Nothing was found for 111111')
-        self.assertFalse(response.context['search_result'])
-        self.assertTemplateUsed('menu/search.html')
-        self.assertTemplateNotUsed('menu/menu.html')
-        self.assertTemplateNotUsed('home/home.html')
+        self.assertEqual(response.context['items'], ['Nothing Was Found'])
+        self.assertTemplateUsed('menu/search_results.html')
+        self.assertTemplateNotUsed('menu/search.html')
 
     def test_search_get_valid_search_data(self):
         response = self.client.get(reverse('menu:search'), data={'search': 'pep'})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['message'], None)
-        self.assertIn(self.product, response.context['search_result'])
-        self.assertTemplateUsed('menu/search.html')
-        self.assertTemplateNotUsed('menu/menu.html')
-        self.assertTemplateNotUsed('home/home.html')
-
-    def test_search_get_valid_search_data_in_category(self):
-        response = self.client.get(reverse('menu:search'), data={'search': 'Foo'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['message'], None)
-        self.assertIn(self.product, response.context['search_result'])
-        self.assertTemplateUsed('menu/search.html')
-        self.assertTemplateNotUsed('menu/menu.html')
-        self.assertTemplateNotUsed('home/home.html')
+        self.assertEqual(self.product.name, response.context['items'][0].name)
+        self.assertTemplateUsed('menu/search_results.html')
