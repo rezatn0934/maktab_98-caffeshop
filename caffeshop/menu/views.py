624da001c3e9b209888611534c09a68f0ff373e2
rezatn0934 <rezatn0934@gmail.com>
2023-08-29 00:37:44 +0330

    chore: define get request in Menu view

--- caffeshop/menu/views.py
+++ caffeshop/menu/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, View
+from django.views.generic import  DetailView, View
 from .models import Product, Category
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models.functions import Greatest
@@ -10,12 +10,24 @@ from django.db.models.functions import Greatest
 class Menu(View):
     template_name = 'menu/menu.html'
     model = Product
-    context_object_name = 'products'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
-        return context
+    def get(self, request):
+        search_term = request.GET.get('search')
+
+        items = self.model.objects.all()
+        if request.headers.get('HX-Request') == 'true':
+            self.template_name = 'menu/search_results.html'
+            if isinstance(search_term, str):
+                if search_term != '':
+                    items = Product.objects.annotate(similarity=Greatest(
+                        TrigramSimilarity("name", string=search_term),
+                        TrigramSimilarity("description", string=search_term),
+                    )).filter(similarity__gt=0).order_by("-similarity")
+
+                    if not items.exists():
+                        items = ["Nothing Was Found"]
+        context = {'items': items, 'categories': Category.objects.all()}
+        return render(request, self.template_name, context=context)
 
 
 class ProductView(DetailView):
