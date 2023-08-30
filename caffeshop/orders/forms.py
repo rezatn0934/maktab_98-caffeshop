29ae97817aaa25f2076d26f40728522641ee3fc2
Farzam <ffarzam_1992@yahoo.com>
2023-08-30 19:22:52 +0330

    refactor: remove styles

--- caffeshop/orders/forms.py
+++ caffeshop/orders/forms.py
@@ -6,12 +6,12 @@ class OrderForm(forms.ModelForm):
     table_number = forms.ModelChoiceField(label="Table Name:(if takeaway, skip this)",
                                           queryset=Table.objects.filter(occupied=False),
                                           required=False, widget=forms.Select(
-            attrs={'class': 'form-control control-label m-0', 'style': 'font-size:14px;'}))
+            attrs={'class': 'form-control control-label text-center'}))
 
     class Meta:
         model = Order
         fields = ["phone_number", "table_number"]
 
         widgets = {"phone_number": forms.TextInput(
-            attrs={"class": "form-control control-label m-0", 'placeholder': 'Phone number Should Start with 09',
-                   'style': 'font-size:14px;'})}
+            attrs={"class": "form-control control-label text-center",
+                   'placeholder': 'Phone number Should Start with 09'})}
