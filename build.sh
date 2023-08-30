5ed8ad7ecaf20696394e4f35b743515fc912d702
rezatn0934 <rezatn0934@gmail.com>
2023-08-31 02:23:29 +0330

    fix: remove build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pi
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
