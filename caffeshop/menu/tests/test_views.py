b8b8c8b1732576f3cc5d809aab3a1464b8b27bf8
rezatn0934 <rezatn0934@gmail.com>
2023-08-30 16:56:12 +0330

    refactor: remove unnecessary tests

--- caffeshop/menu/tests/test_views.py
+++ caffeshop/menu/tests/test_views.py
@@ -42,38 +42,3 @@ class TestMenuView(TestCase):
         self.assertEqual(response.context['items'], ['Nothing Was Found'])
         self.assertTemplateUsed('menu/search_results.html')
         self.assertTemplateNotUsed('menu/search.html')
-
-# class TestSearchView(TestCase):
-#
-#     def setUp(self):
-#         self.image = open(settings.MEDIA_ROOT / "images/test/pina_colada.png", 'rb').read()
-#         self.category = Category.objects.create(name='Food')
-#         self.product = Product.objects.create(name='pepperoni', category=self.category, price=100,
-#                                               image=SimpleUploadedFile.from_dict(
-#                                                   {'filename': 'product_pic.png', 'content': self.image,
-#                                                    'content_tye': 'image/png'})
-#                                               )
-#         self.client = Client()
-#
-#     def tearDown(self):
-#         self.product.delete()
-#         self.category.delete()
-#
-#     def test_search_get_no_data(self):
-#         response = self.client.get(reverse('menu:search'))
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.context['items'], [])
-#         self.assertTemplateUsed('menu/search.html')
-#
-#     def test_search_get_invalid_search_data(self):
-#         response = self.client.get(reverse('menu:search'), data={'search': '111111'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(response.context['items'], ['Nothing Was Found'])
-#         self.assertTemplateUsed('menu/search_results.html')
-#         self.assertTemplateNotUsed('menu/search.html')
-#
-#     def test_search_get_valid_search_data(self):
-#         response = self.client.get(reverse('menu:search'), data={'search': 'pep'})
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(self.product.name, response.context['items'][0].name)
-#         self.assertTemplateUsed('menu/search_results.html')
